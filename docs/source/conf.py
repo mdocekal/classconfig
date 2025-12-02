8c64c084c162321c8e679d913b273891ca9a4ab6
Martin Dočekal <docekmartin@gmail.com>
2025-12-02 14:13:53 +0100

    docs: Add `sphinx_rtd_theme` to Sphinx configuration.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,8 @@
 # Configuration file for the Sphinx documentation builder.
 import os
 import sys
+import sphinx_rtd_theme
+
 sys.path.insert(0, os.path.abspath('../..'))
 
 project = 'classconfig'
@@ -12,12 +14,14 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
 
 html_theme_options = {
