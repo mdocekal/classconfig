5d12a3f843bbf2c90b81c8780c69f71586253bed
Martin Dočekal <docekmartin@gmail.com>
2025-12-03 16:15:38 +0100

    repository update

--- release.sh
+++ release.sh
@@ -3,4 +3,4 @@ rm -rf dist
 python -m pip install --upgrade build
 python -m build
 python -m pip install --user --upgrade twine
-python -m twine upload dist/*
+python -m twine upload dist/* --repository classconfig
